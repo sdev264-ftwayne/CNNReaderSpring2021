66ec49b8ce844e0cc15666191688a3c149ac6bc9
Angela K Venable <avenable11@ivytech.edu>
2021-04-29 10:17:23 -0400

    Fixed WebView so it doesn't open the external browser.

--- app/src/main/java/edu/ivytech/cnnreader/ArticleWebActivity.kt
+++ app/src/main/java/edu/ivytech/cnnreader/ArticleWebActivity.kt
@@ -3,6 +3,7 @@ package edu.ivytech.cnnreader
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import edu.ivytech.cnnreader.databinding.ActivityArticleWebBinding
 
 class ArticleWebActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class ArticleWebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityArticleWebBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.articleWebView.webViewClient = WebViewClient()
         val intent = intent
         val link:String? = intent.getStringExtra("link")
         binding.articleWebView.loadUrl(link!!)
